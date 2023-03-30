c781d0b825ace6617ef102d3ee73ff25e0acddb3
root <jclaris6@gmail.com>
2023-03-30 05:44:18 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/**
- * bmain - function executed before main
- * Return: no return.
- */
+void theHare(void)__attribute__((constructor));
 
-void __attribute__((constructor)) bmain()
+/**
+ * theHare - prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ */
+void theHare(void)
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
