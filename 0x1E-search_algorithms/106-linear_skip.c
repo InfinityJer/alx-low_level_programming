ebc55b18b9cfea3453af2a636736f681d2ce6480
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-10 18:58:42 +0800

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,11 +1,14 @@
-#include "search_algos.h"
+include "search_algos.h"
 
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers.
- * @list: A pointer to the head of the skip list to search in.
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
  * @value: The value to search for.
  *
- * Return: A pointer to the first node where value is located, or NULL if not found.
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
  * Description: Prints a value every time it is compared in the list.
  *              Uses the square root of the list size as the jump step.
  */
