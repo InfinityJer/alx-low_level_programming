23531120950f997714ae989606744e7778d682fc
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-10 18:17:40 +0800

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,27 +13,33 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, start = 0, end = sqrt(size);
+	int index, m, k, prev;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	while (array[end] < value && end < size)
-	{
-		printf("Value checked array[%d] = [%d]\n", (int)(start), (int)array[start]);
-		if (array[end] == value)
-			return (end);
-		start = end;
-		end += sqrt(size);
-	}
-	printf("Value checked array[%d] = [%d]\n", (int)(start), (int)array[start]);
-	printf("Value found between indexes [%d] and [%d]\n", (int)start, (int)end);
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
 
-	for (i = start; i <= end && i < size; i++)
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)i, (int)array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
+
 	return (-1);
 }
