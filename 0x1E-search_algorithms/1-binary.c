56f3a2fdf89e3111bc9e31476fa661de9f67ff0c
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-10 17:49:21 +0800

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,37 +10,58 @@
  * Return: If the value is not present or the array is NULL, -1.
  *         Otherwise, the index where the value is located.
  */
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = low; i < high; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
+  * recursive_search_binary - Searches recursively for a value in a sorted
+  *                           array of integers using binary search.
+  * @array: A pointer to the first element of the sub-array to search.
+  * @low: The starting index of the sub-array to search.
+  * @high: The ending index of the sub-array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the sub-array being searched after each change
+  */
+int recursive_search_binary(int *array, size_t low, size_t high, int value)
+{size_t mid;
+
+	if (high < low)
+		return (-1);
+
+	print_array(array, low, high);
+	mid = low + (high - low) / 2;
+	if (array[mid] == value)
+		return (mid);
+	if (array[mid] > value)
+		return (recursive_search_binary(array, low, mid - 1, value));
+	return (recursive_search_binary(array, mid + 1, high, value));
+}
+
+/**
+* binary_search - Searches for a value in a soarted array of integers
+*   using Binary Search Algorithm
+* @array: A pointer to the first element of the array to search in
+* @size: The number of elements in the array
+* @value: The value to search for
+*
+* Return: If the value is not present or the array is NULL, -1.
+*         Otherwise, the first index where the value is located.
+*/
 int binary_search(int *array, size_t size, int value)
 {
-    int left, right, mid;
+	if (array == NULL)
+		return (-1);
 
-    if (array == NULL)
-        return (-1);
-
-    left = 0;
-    right = size - 1;
-
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (int i = left; i <= right; i++)
-        {
-            if (i == right)
-                printf("%d", array[i]);
-            else
-                printf("%d, ", array[i]);
-        }
-        printf("\n");
-
-        mid = (left + right) / 2;
-
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return (-1); /* Value not found */
+	return (recursive_search_binary(array, 0, size - 1, value));
 }
